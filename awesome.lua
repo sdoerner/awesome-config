1b62a2af15d1cf056181e17914ecd6ca1b72ab0d
Sebastian Dörner <sebastian@sebastian-doerner.de>
2012-07-06 12:35:30 +0100

    Add workaround for focus bug in awesome 3.4.12
    See https://awesome.naquadah.org/bugs/index.php?do=details&task_id=1011

--- awesome.lua
+++ awesome.lua
@@ -400,7 +400,7 @@ awful.rules.rules =  {
     { rule = { },
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
-                     focus = true,
+                     callback = function(c) if awful.client.focus.filter(c) then client.focus = c end end,
                      keys = clientkeys,
                      buttons = clientbuttons } },
   { rule =
